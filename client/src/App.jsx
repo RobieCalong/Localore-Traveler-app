480bb24778ae3b5d0e440dd4e699c28deb36fb96
robC_vW <robiecalong2@gmail.com>
2025-09-04 13:34:23 -0500

    created Homepage Navigation Map and simple Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { Routes, Route } from "react-router";
+
+import Homepage from "./components/Homepage";
+import Navigation from "./components/Navigation";
+import Map from "./components/Map";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>app component works</h1>
-      </div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
     </>
   );
 }
