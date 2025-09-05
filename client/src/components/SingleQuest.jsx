0a7ce5a5e209f1e5cd487f43ccedfee6663b471e
robC_vW <robiecalong2@gmail.com>
2025-09-05 06:01:26 -0500

    added Components SingleQuest NpcBob BubbleText

--- client/src/components/SingleQuest.jsx
+++ client/src/components/SingleQuest.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import QuestContext from "./QuestContext";
+import NpcBob from "./NpcBob";
 
 function SingleQuest() {
-  return <div>SingleQuest</div>;
+  const [say, setSay] = useState("");
+
+  const { quest } = useContext(QuestContext);
+
+  return (
+    <div>
+      <h1>{quest.title}</h1>
+      <NpcBob say={say} />
+    </div>
+  );
 }
 
 export default SingleQuest;
