132329e416f328f92340feafc3e6b41d93268011
jshirey55 <jshirey5555@gmail.com>
2025-09-08 12:38:56 -0400

    fixed level seeding

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -1,18 +1,18 @@
 import db from "#db/client";
 import bcrypt from "bcrypt";
 
-export async function createUser(username, password) {
+export async function createUser(username, password, experience, level_id) {
   const sql = `
   INSERT INTO users
-    (username, password)
+    (username, password, experience, level_id)
   VALUES
-    ($1, $2)
+    ($1, $2, $3, $4)
   RETURNING *
   `;
   const hashedPassword = await bcrypt.hash(password, 10);
   const {
     rows: [user],
-  } = await db.query(sql, [username, hashedPassword]);
+  } = await db.query(sql, [username, hashedPassword, experience, level_id]);
   return user;
 }
 
