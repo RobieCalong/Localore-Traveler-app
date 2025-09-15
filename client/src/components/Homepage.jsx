bcecc81592b0fe103cb987234d82450e123ac32d
jshirey55 <jshirey5555@gmail.com>
2025-09-15 08:58:07 -0400

    gets stuck when tying to upload level to home page

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import FetchLevel from "./FetchLevel";
 
 function Homepage() {
   return (
     <div>
       <h1>Homepage: Welcome to Localore</h1>
+      <FetchLevel level={1} />
     </div>
   );
 }
