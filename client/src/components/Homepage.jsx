2d433786952cbe177899961358d7c4021452d19f
jshirey55 <jshirey5555@gmail.com>
2025-09-15 10:11:32 -0400

    removed

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import FetchLevel from "./FetchLevel";
 
 function Homepage() {
   return (
     <div>
       <h1>Homepage: Welcome to Localore</h1>
-      <FetchLevel level={1} />
     </div>
   );
 }
